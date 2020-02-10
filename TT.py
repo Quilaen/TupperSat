bfc98619a7bcbcce93bcbe7661e22206f115a5af
Matt <c17710119@mytudublin.ie>
2020-02-10 15:48:40 +0000

    Updated TT.py -- including a sample __main__

--- TT.py
+++ TT.py
@@ -1,22 +1,36 @@
-def init():
-	from TT import tempInit
-	from TT import temp
-	from TT import getExtTemp
-	from TT import getIntTemp
-	from log import log
-
 def tempInit():
 	open("/sys/bus/w1/devices/28-0417c2b877ff/w1_slave")
 	open("/sys/bus/w1/devices/28-00000bc26d08/w1_slave")
 
 def temp(location):
-	fullstr = open( "/sys/bus/w1/devices/" + location + "/w1_slave", "r").readlines()
-	pos = fullstr[1].find('t=')
-	return float(fullstr[1][pos+2:])/1000.0
-	print float(fullstr[1][pos+2:])/1000.0
+	f = open( "/sys/bus/w1/devices/"+location+"/w1_slave", "r").readlines()
+	pos = f[1].find('t=')
+	d = float(f[1][pos+2:])/1000.0
+	print(d)
+#	f.flush()
+	return d
 
 def getExtTemp():
 	temp('28-0417c2b877ff')
 
 def getIntTemp():
 	temp('28-00000bc26d08')
+
+
+import logging
+
+
+def main():
+    logging.basicConfig(filename = "/home/pi/logs/Log01", level=logging.INFO, format= '%(asctime)s %(levelname)s: %(message)s')
+
+    # do some stuff
+    while True:
+          In = getIntTemp()
+          Out = getExtTemp()
+          logging.info('Ext Temp: %f', Out)
+          logging.info('Int Temp: %f', In)
+          print("Ext:Int Temperatures are: "+Out+ ":"+In)
+
+
+if __name__=="__main__":
+	main()
