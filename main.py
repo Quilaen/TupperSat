9b823e8971073633ddc61622d4d6db7fd97d646c
Quilaen <Quilaen@users.noreply.github.com>
2020-02-11 14:08:44 +0000

    Addended First draft of a main file

--- main.py
+++ main.py
@@ -38,7 +38,11 @@ class sensor:
     def measure(self):
         while True:
             try:
-                # radioFunction("preamble|"+self.extTemp()+"|"+self.intTemp()+"|"+self.GPS()+"|"+self.pres())
+                # myradio.send_telemetry(datetime.datetime.now(), GPS(), intTemp(), extTemp(), pres() )
+                # GPS() gives lattitude, longitude, horizontal dilution, altitude
+                # intTemp() gives the internal temperature
+                # extTemp() gives the external temperature
+                # pres() gives the pressure
                 # when radioFunction() is working, the below can be commented out, as the logging should happen as
                 # the radioFunction() calls the functions
                 self.extTemp()  # call external temperature & log it
@@ -51,30 +55,43 @@ class sensor:
 
     def extTemp(self):
         if self.ext:  # checking if we want to read the external
-            extM = read(self.extn, "r").readlines()  # reading the lines
+            self.ext.type2.seek(0)  # goes to the start of the ext temp file
+            extM = self.ext.type2.readlines()  # reading the lines
             extP = extM[1].find('t=')  # locating the temperature
             ext = float(extM[1][extP+2:])/1000.0  # converting the temperature
             logging.info('Ext Temp: %f', ext)  # logging the data
-            return ext  # returns the temperature for use in the radioFunction()
+            return ext or None # returns the temperature for use in the radioFunction()
 
     def intTemp(self):
         if self.int:  # checking if we want to read the internal temp
-            intM = read(self.intn, "r").readlines()  # reading the lines
+            self.int.type2.seek(0) #goes to the start of the int temp file
+            intM = self.int.type2.readlines()  # reading the lines
             intP = intM[1].find('t=')  # locating the temperature
             int = float(intM[1][intP+2:])/1000.0  # converting the temperature
             logging.info('Int Temp: %f', int)  # logging the data
-            return int  # returns the temperature for use in the radioFunction()
+            return int or None # returns the temperature for use in the radioFunction()
 
     def GPS(self):
         if self.GPS:  # checking if we want to read he GPS
             sentence = self.gpsn.readline().decode('utf8')  # reading the gps data & decoding
             sentenceid = sentence[3:6]  # parsing out the prefix
-            if sentenceid == 'GLL':  # if it is the GPS data we want
+            if sentenceid == 'GGA':  # if it is the GPS data we want
                 GPSData = sentence.split(",")  # split the sentence to allow the use of the data
-                gps = (GPSData[1] + GPSData[2] + ', ' + GPSData[3] + GPSData[4])  # combine the data
+                gps = (GPSData[2] + GPSData[3] + ', ' + GPSData[4] + GPSData[5] + ', '+GPSData[8]+ ',' +GPSData[9]+ ' m')
+                # combine the data
+                # [2] & [3] are latitude and hemisphere N/S
+                # [4] & [5] are longitude and hemisphere E/W
+                # [8] is horizontal dilution
+                # [9] is the altitude in meters.
                 logging.info('GPS Data: %W', gps)  # log the data
-                return gps  # returns the GPS data -- this may be better using GPSData variable depending
-                            # on the requirements of the radio
+                if GPSData[2] == "N" and GPSData[4] == "W":  # checking the hemispheres
+                    return float(GPSData[2]), float(GPSData[4]*-1), float(GPSData[8]), float(GPSData[9]) or None
+                elif GPSData[2] == "N" and GPSData[4] == "E":  # checking the hemispheres
+                    return float(GPSData[2]), float(GPSData[4]), float(GPSData[8]), float(GPSData[9]) or None
+                elif GPSData[2] == "S" and GPSData[4] == "W":  # checking the hemispheres
+                    return float(GPSData[2]*-1), float(GPSData[4]*-1), float(GPSData[8]), float(GPSData[9]) or None
+                else:  # if none of the above
+                    return float(GPSData[2]*-1), float(GPSData[4]), float(GPSData[8]), float(GPSData[9]) or None
 
     def pres(self):
         if self.pressure:  # checking if we want to read the pressure systems
@@ -92,7 +109,7 @@ class sensor:
             presadc = (presadcbytes[0] << 16)+(presadcbytes[1] << 8)+(presadcbytes[2])  # pressure reading convert
             logging.info('Pressure: %f', presadc)  # logging the pressure
             logging.info('Temp (P): %f', tempadc)  # logging the temperature
-            return presadc, tempadc  # returning the pressure and temp data for the radioFunction() to use
+            return presadc or None # returning the pressure data for the radioFunction() to use
 
 
 if __name__ == "__main__":  # runs the below only when testing
