b2da3856e48d0d105af669692fbfce737964554c
Matt <c17710119@mytudublin.ie>
2020-02-12 10:16:56 +0000

    Updated the main.py it returns logs of the temp & pressure

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import logging
-# import smbus
+import smbus
 import time
-# import serial
+import serial
 
 
 class sensor:
     def __init__(self, ext, int, gps, pressure):
-        logging.basicConfig(filename="Test", level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+        logging.basicConfig(filename="DryRun01", level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
         # setting up the logging session
         self.ext = ext  # setting the toggle for recording external temp
         self.extn = "/sys/bus/w1/devices/28-0417c2b877ff/w1_slave",  # setting the location of the external temp device
-#        self.ext.type2 = open("/sys/bus/w1/devices/28-0417c2b877ff/w1_slave", "r")  # opening the external temp file
+        self.exto = open("/sys/bus/w1/devices/28-0417c2b877ff/w1_slave", "r")  # opening the external temp file
         self.int = int  # setting the toggle for recording internal temp
         self.intn = "/sys/bus/w1/devices/28-00000bc26d08/w1_slave",  # setting the location of the internal temp device
-#        self.int.type2 = open("/sys/bus/w1/devices/28-00000bc26d08/w1_slave", "r")  # opening the internal tmep file
+        self.into = open("/sys/bus/w1/devices/28-00000bc26d08/w1_slave", "r")  # opening the internal tmep file
         self.gps = gps  # setting the toggle for recording GPS
-#        self.gpsn = serial.Serial('COM5', 9600) #setting location of the GPS module
+        self.gpsn = serial.Serial('/dev/ttyACM0', 9600) #setting location of the GPS module
         self.pressure = pressure  # setting the toggle for recording pressure/temp
-#        self.pressuren = "0x77" #setting location of the pressure module
+        self.pressuren = 0x77 #setting location of the pressure module
         if self.ext:  # if reading external temp
             logging.info("Logging External Temperature.")  # make a record of it
         else:  # otherwise
@@ -47,7 +47,7 @@ class sensor:
                 # the radioFunction() calls the functions
                 self.extTemp()  # call external temperature & log it
                 self.intTemp()  # call internal temperature & log it
-                self.GPS()  # call GPS data & log it
+#                self.GPS()  # call GPS data & log it
                 self.pres()  # call pressure data & log it
                 time.sleep(2.0)  # waits for 2 seconds to run again
             except KeyboardInterrupt:  # allows for the system to be interrupted
@@ -55,8 +55,8 @@ class sensor:
 
     def extTemp(self):
         if self.ext:  # checking if we want to read the external
-            self.ext.type2.seek(0)  # goes to the start of the ext temp file
-            extM = self.ext.type2.readlines()  # reading the lines
+            self.exto.seek(0)  # goes to the start of the ext temp file
+            extM = self.exto.readlines()  # reading the lines
             extP = extM[1].find('t=')  # locating the temperature
             ext = float(extM[1][extP+2:])/1000.0  # converting the temperature
             logging.info('Ext Temp: %f', ext)  # logging the data
@@ -64,8 +64,8 @@ class sensor:
 
     def intTemp(self):
         if self.int:  # checking if we want to read the internal temp
-            self.int.type2.seek(0) #goes to the start of the int temp file
-            intM = self.int.type2.readlines()  # reading the lines
+            self.into.seek(0) #goes to the start of the int temp file
+            intM = self.into.readlines()  # reading the lines
             intP = intM[1].find('t=')  # locating the temperature
             int = float(intM[1][intP+2:])/1000.0  # converting the temperature
             logging.info('Int Temp: %f', int)  # logging the data
@@ -101,16 +101,17 @@ class sensor:
             time.sleep(0.05)  # pause to allow completion of previous step
             tempadcbytes = bus.read_i2c_block_data(addr, 0x00)  # temperature ADC bytes
             time.sleep(0.05)  # pause to allow completion of previous step
-            tempadc = tempadcbytes[0] * 65536.0 + tempadcbytes[1] * 256.0 + tempadcbytes[2]  # temp reading convert
+            tempadc = (tempadcbytes[0] * 65536.0) + (tempadcbytes[1] * 256.0) + (tempadcbytes[2])  # temp reading convert
             bus.write_byte(addr, 0x48)  # writing a new bite to the bus
             time.sleep(0.05)  # pause to allow completion of previous step
             presadcbytes = bus.read_i2c_block_data(addr, 0x00)  # pressure ADC bytes
             time.sleep(0.05)  # pause to allow completion of previous step
             presadc = (presadcbytes[0] << 16)+(presadcbytes[1] << 8)+(presadcbytes[2])  # pressure reading convert
             logging.info('Pressure: %f', presadc)  # logging the pressure
-            logging.info('Temp (P): %f', tempadc)  # logging the temperature
+#            logging.info('Temp (P): %f', tempadc)  # logging the temperature
             return presadc or None # returning the pressure data for the radioFunction() to use
 
 
 if __name__ == "__main__":  # runs the below only when testing
-    Sensor = sensor(True, False, False, False)  # sets the external reading, and none of the others.
+	Sensor = sensor(True, True, True, True)  # sets the external reading, and none of the others.
+	Sensor.measure()
